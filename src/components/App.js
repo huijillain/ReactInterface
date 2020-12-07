6661996642aeeec08ea9f0144147eed7bd7f2790
Sky <skyjillain@gmail.com>
2020-12-07 16:09:05 -0700

    adjust files/delete files not used for this course

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../css/App.css";
+import React from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My React App: 06DEC2020</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
