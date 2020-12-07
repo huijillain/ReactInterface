d4b1783cb0f0d08ce7372cda02a5ffb2546d72ab
Sky <skyjillain@gmail.com>
2020-12-07 16:52:06 -0700

    Creating subcomponents

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import "../css/App.css";
-import React from "react";
+import React, { Component } from "react";
+import AddAppointments from "./AddAppointments";
+import SearchAppointments from "./SearchAppointments";
+import ListAppointments from "./ListAppointments";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>My React App: 06DEC2020</h1>
-        </header>
-      </div>
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {/* <div>Add Appointments</div> 
+                    <div>Search Appointments</div>
+                    <div>List Appointments</div> */}
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     );
   }
 }
